b17caa6025b9530ed585406ff2f1b4ef1357f1c9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-02 09:29:42 +0530

    initial run and automatic updates to files.

--- backend/src/main/java/com/ecommercesystem/backend/BackendApplication.java
+++ backend/src/main/java/com/ecommercesystem/backend/BackendApplication.java
@@ -2,12 +2,21 @@ package com.ecommercesystem.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RequestMapping("")
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@GetMapping("")
+	public String showAllUsersPage(){
+		return "users";
+	}
 }
--- backend/src/main/java/com/ecommercesystem/backend/ShutdownController.java
+++ backend/src/main/java/com/ecommercesystem/backend/ShutdownController.java
@@ -1,13 +1,10 @@
 package com.ecommercesystem.backend;
 
-
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 public class ShutdownController implements ApplicationListener<ServletWebServerInitializedEvent> {
