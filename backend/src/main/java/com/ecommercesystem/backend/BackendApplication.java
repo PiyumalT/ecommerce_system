88de829bbb3520988c21b7ef9a7c5cde28bd20a2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-13 19:57:46 +0530

    made a clean slate for implement jwt security.

--- backend/src/main/java/com/ecommercesystem/backend/BackendApplication.java
+++ backend/src/main/java/com/ecommercesystem/backend/BackendApplication.java
@@ -1,19 +1,10 @@
 package com.ecommercesystem.backend;
 
-import com.ecommercesystem.backend.controller.UserController;
-import com.ecommercesystem.backend.model.User;
-import com.ecommercesystem.backend.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
+
+
 
 @SpringBootApplication
 public class BackendApplication {
