20ba0122f4e91e609ce60b59d1387b49edb4b58e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-13 19:57:26 +0530

    made a clean slate for implement jwt security.

--- backend/src/main/java/com/ecommercesystem/backend/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommercesystem.backend.repository;
-
-import com.ecommercesystem.backend.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    // all crud operations handle here - no need to add - already defined
-}
