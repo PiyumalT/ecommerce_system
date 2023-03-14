e47bfb1ef13b0116f1222088c03c532f4a6dbc44
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-14 13:33:49 +0530

    add auth controller.

---
+++ backend/src/main/java/com/ecommercesystem/backend/auth/AuthenticationController.java
@@ -0,0 +1,40 @@
+package com.ecommercesystem.backend.auth;
+
+import com.ecommercesystem.backend.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService service;
+
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request, Role role) {
+        final AuthenticationResponse token = service.register(request, role);
+        if (token == null) {
+            return ResponseEntity.badRequest().body("email already exists");
+        } else {
+            return ResponseEntity.ok(token);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest request) {
+        return register(request, Role.USER);
+    }
+
+    @PostMapping("/registerAdmin")
+    public ResponseEntity<?> registerAdmin(@RequestBody RegisterRequest request) {
+        return register(request, Role.ADMIN);
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
