a9e5f9261030f132e4dc7c1a9c9f78afdaa70fbd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-14 16:26:35 +0530

    add revoke and expire token on log out to log out service class.

--- backend/src/main/java/com/ecommercesystem/backend/config/LogoutService.java
+++ backend/src/main/java/com/ecommercesystem/backend/config/LogoutService.java
@@ -1,5 +1,6 @@
 package com.ecommercesystem.backend.config;
 
+import com.ecommercesystem.backend.token.TokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -11,12 +12,27 @@ import javax.servlet.http.HttpServletResponse;
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
+    private final TokenRepository tokenRepository;
+
     @Override
     public void logout(
             HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication
     ) {
-
+        final String authHeaderStartWord = "Bearer ";
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith(authHeaderStartWord)) {
+            return;
+        }
+        jwt = authHeader.substring(authHeaderStartWord.length());
+        var storedToken = tokenRepository.findByToken(jwt)
+                .orElse(null);
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+        }
     }
 }
