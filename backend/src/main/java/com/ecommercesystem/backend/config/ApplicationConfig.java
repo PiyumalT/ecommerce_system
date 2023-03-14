b8ecc1d076ca2878f625d38c02b1aed2ff7ee4ee
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-14 10:15:26 +0530

    add app config file.

---
+++ backend/src/main/java/com/ecommercesystem/backend/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.ecommercesystem.backend.config;
+
+import com.ecommercesystem.backend.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("the entered email not found on the database."));
+    }
+}
