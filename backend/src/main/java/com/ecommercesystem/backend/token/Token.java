4888ed05087176139501d1ad6f766e8a997fa8c8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-14 14:13:07 +0530

    add token class.

---
+++ backend/src/main/java/com/ecommercesystem/backend/token/Token.java
@@ -0,0 +1,28 @@
+package com.ecommercesystem.backend.token;
+
+import com.ecommercesystem.backend.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+    private boolean expired;
+    private boolean revoked;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
