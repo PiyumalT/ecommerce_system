2a49009ab62b18df216304a69fa02e24aa5a3340
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-14 09:34:33 +0530

    add jwt auth filter class.

---
+++ backend/src/main/java/com/ecommercesystem/backend/config/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.ecommercesystem.backend.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String AUTH_HEADER_START_STRING = "Bearer ";
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith(AUTH_HEADER_START_STRING)){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(AUTH_HEADER_START_STRING.length());
+        userEmail = jwtService.extractUsername(jwt);
+    }
+}
