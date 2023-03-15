1583abbd692c877155599e93201cd67d9749672c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-15 16:40:48 +0530

    add verification mail sending method, verify from that mail method and an environmental variable repository.

--- backend/src/main/java/com/ecommercesystem/backend/auth/AuthenticationService.java
+++ backend/src/main/java/com/ecommercesystem/backend/auth/AuthenticationService.java
@@ -8,21 +8,31 @@ import com.ecommercesystem.backend.user.Role;
 import com.ecommercesystem.backend.user.User;
 import com.ecommercesystem.backend.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import net.bytebuddy.utility.RandomString;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
-
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final JavaMailSender mailSender;
+    private final Environment env;
 
     private void saveUserToken(User user, String jwtToken) {
         var token = Token.builder()
@@ -35,23 +45,59 @@ public class AuthenticationService {
         tokenRepository.save(token);
     }
 
-    public AuthenticationResponse register(RegisterRequest request, Role role) {
+    private void sendVerificationEmail(User user, String siteURL) throws MessagingException, UnsupportedEncodingException {
+        String toSendAddress = user.getEmail();
+        String fromSendingAddress = env.getProperty("SENDER_MAIL");
+        String senderName = "Group 04";
+        String subject = "Please verify your registration.";
+        String content = "Dear [[name]], <br>"
+                + "Please click the link link below to verify your registration;<br>"
+                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
+                + "Thank you, <br>"
+                + "Group 04."
+                + "<h6>Please login inorder to acquire the authorization to access the site.</h6>";
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        assert fromSendingAddress != null;
+        helper.setFrom(fromSendingAddress, senderName);
+        helper.setTo(toSendAddress);
+        helper.setSubject(subject);
+        content = content.replace("[[name]]", user.getFirstname());
+        String verifyURL = siteURL + "/verify?code=" + user.getVerificationCode();
+        content = content.replace("[[URL]]", verifyURL);
+        helper.setText(content, true);
+        mailSender.send(message);
+    }
+
+    public boolean verify(String verificationCode) {
+        User user = userRepository.findUserByVerificationCode(verificationCode)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found in the database or this account is already verified."));
+        if (user == null || user.isEnabled()) {
+            return false;
+        }
+        user.setVerificationCode(null);
+        user.setEnabled(true);
+        userRepository.save(user);
+        return true;
+    }
+
+    public String register(RegisterRequest request, Role role, String siteURL) throws MessagingException, UnsupportedEncodingException {
         if (userRepository.existsByEmail(request.getEmail())) {
             return null;
         }
+        String randomVerificationCode = RandomString.make(64);
         var user = User.builder()
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .email(request.getEmail())
                 .role(role)
+                .enabled(false)
+                .verificationCode(randomVerificationCode)
                 .build();
         var savedUser = userRepository.save(user);
-        var jwtToken = jwtService.generateToken(user);
-        saveUserToken(savedUser, jwtToken);
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
+        sendVerificationEmail(savedUser, siteURL);
+        return "Verification email has sent to your inbox.";
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
@@ -62,7 +108,10 @@ public class AuthenticationService {
                 )
         );
         var user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException("email not found."));
+                .orElseThrow(() -> new UsernameNotFoundException("user not found."));
+        if (!user.isEnabled()) {
+            throw new AuthenticationServiceException("Account not verified");
+        }
         var jwtToken = jwtService.generateToken(user);
         revokeUserTokens(user);
         saveUserToken(user, jwtToken);
