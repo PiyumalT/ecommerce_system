66ace0e3a0f88e8c16bdadb72c3d1c569f8554c7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-15 16:35:14 +0530

    add method to get users by the verification code.

--- backend/src/main/java/com/ecommercesystem/backend/user/UserRepository.java
+++ backend/src/main/java/com/ecommercesystem/backend/user/UserRepository.java
@@ -8,4 +8,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findUserByVerificationCode(String verificationCode);
 }
