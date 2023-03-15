e96c2e6b4d3c5b17ebf0ab539db1013f6c00a217
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-15 11:28:01 +0530

    add preauthorize annotation to filter the viewing only user profile.

--- backend/src/main/java/com/ecommercesystem/backend/user/UserController.java
+++ backend/src/main/java/com/ecommercesystem/backend/user/UserController.java
@@ -2,19 +2,23 @@ package com.ecommercesystem.backend.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService service;
+    private final UserService userService;
 
+    @PreAuthorize("#id.equals(authentication.principal.id)")
     @GetMapping("/{id}")
-    public ResponseEntity<?> viewUserById(@PathVariable Integer id) {
-        return ResponseEntity.ok(service.getUserById(id));
+    public ResponseEntity<?> viewUserById(@PathVariable("id") Integer id, HttpServletRequest request) {
+        return ResponseEntity.ok(userService.getUserById(id, request));
     }
 }
