ba5e13289fe01cbd87f56b8221196d1c3232edeb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-16 20:21:37 +0530

    add exception handler for controllers.

---
+++ backend/src/main/java/com/ecommercesystem/backend/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.ecommercesystem.backend.exceptionHandler;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    final String JWT_EXPIRE_MESSAGE = "The validate token issued to you has expired.";
+
+    @ExceptionHandler({UsernameNotFoundException.class, EntityNotFoundException.class})
+    public ResponseEntity<?> handleUsernameNotFoundException() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<?> handleExpiredJwtException() {
+        return ResponseEntity.badRequest().body(JWT_EXPIRE_MESSAGE);
+    }
+}
