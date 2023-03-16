34ba6db5f22db378acf63b3b63585808b908c141
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-16 20:23:36 +0530

    add user dto class.

---
+++ backend/src/main/java/com/ecommercesystem/backend/user/UserDto.java
@@ -0,0 +1,19 @@
+package com.ecommercesystem.backend.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
+
