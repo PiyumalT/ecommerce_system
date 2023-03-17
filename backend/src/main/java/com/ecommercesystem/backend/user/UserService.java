c6f9817e9a86bdb3f157fb2fe76481b9265131dd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-17 15:14:12 +0530

    wrap entity object with a DTO, get service from token service, add save user for test method.

--- backend/src/main/java/com/ecommercesystem/backend/user/UserService.java
+++ backend/src/main/java/com/ecommercesystem/backend/user/UserService.java
@@ -1,30 +1,51 @@
 package com.ecommercesystem.backend.user;
 
+import com.ecommercesystem.backend.config.JwtService;
+import com.ecommercesystem.backend.token.TokenService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-//    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final TokenService tokenService;
+    private final ModelMapper modelMapper;
+    private final UserMapper usermapper;
 
-    public final User getUserById(Integer id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+    public final String updateUserById(Integer id, UserDTO editedUser) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("No User name found to id: " + id + ". "));
+        return updateUser(user, editedUser);
     }
 
-//    public final String updateUserById(Integer id, UserDto editedUser) {
-//        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("No User name found to id: " + id + ". "));
-//        return updateUser(user, editedUser);
-//    }
-//
-//    public final String updateUser(User user, UserDto editedUser) {
-//        userMapper.updateUserFromDto(editedUser, user);
-//        userRepository.save(user);
-//        return "";
-//    }
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(usermapper)
+                .collect(Collectors.toList());
+    }
+
+    public final String updateUser(User user, UserDTO editedUser) {
+        user.setFirstname(editedUser.firstname());
+        user.setLastname(editedUser.lastname());
+        userRepository.updateUser(editedUser.firstname(), editedUser.lastname(), user.getId());
+        return "updated " + user.getFirstname() + " " + user.getLastname();
+    }
+
+
+    public final UserDTO getUserById(Integer id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+        return modelMapper.map(user, UserDTO.class);
+    }
 
     public final String deactivateUser(User user) {
         user.setEnabled(false);
@@ -39,11 +60,29 @@ public class UserService {
 
     public final String removeUserFromDB(User user) {
         userRepository.delete(user);
+        tokenService.revokeUserTokens(user);
         return "deactivated " + user.getFirstname() + " " + user.getLastname();
     }
 
     public final String removeUserFromDBById(Integer id) {
         User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("No User name found to id: " + id + ". "));
         return removeUserFromDB(user);
+    }
+
+    public String saveUserForTest() {
+        var user = User.builder()
+                .firstname("Nayantha")
+                .lastname("Yasiru")
+                .password(this.passwordEncoder.encode("1234"))
+                .email("nayanthayasiru@gmail.com")
+                .role(Role.USER)
+                .enabled(true)
+                .verificationCode(null)
+                .build();
+        var savedUser = userRepository.save(user);
+        var jwtToken = jwtService.generateToken(savedUser);
+        tokenService.revokeUserTokens(savedUser);
+        tokenService.saveUserToken(savedUser, jwtToken);
+        return jwtToken;
     }
 }
