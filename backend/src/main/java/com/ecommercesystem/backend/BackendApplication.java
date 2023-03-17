55c99f9e6508d6faa209fecd3c2dd2b7ff13ea4f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-17 15:02:11 +0530

    reformat the file.

--- backend/src/main/java/com/ecommercesystem/backend/BackendApplication.java
+++ backend/src/main/java/com/ecommercesystem/backend/BackendApplication.java
@@ -3,14 +3,10 @@ package com.ecommercesystem.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
