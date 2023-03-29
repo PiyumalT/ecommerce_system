e267d498ba480dafe651057678219491f8979991
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-29 19:00:35 +0530

    remove no args constructor.

--- backend/src/main/java/com/ecommercesystem/backend/user/UserDTO.java
+++ backend/src/main/java/com/ecommercesystem/backend/user/UserDTO.java
@@ -1,12 +1,10 @@
 package com.ecommercesystem.backend.user;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
     private Integer id;
     private String firstname;
