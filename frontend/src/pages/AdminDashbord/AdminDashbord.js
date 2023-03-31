1b6f3ba885873426f8cf302644e18a3788801e37
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-03-31 21:37:26 +0530

    AdminDashbord page updated

--- frontend/src/pages/AdminDashbord/AdminDashbord.js
+++ frontend/src/pages/AdminDashbord/AdminDashbord.js
@@ -1,35 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './adminDashbord.css';
 
+export default function AdminDashboard() {
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+  const [todayOrders, setTodayOrders] = useState(0);
+  const [todayIncome, setTodayIncome] = useState(0);
 
-export default function AdminDashbord() {
+  useEffect(() => {
+    getTotalUsers();
+    getTotalItems();
+    getTodayOrders();
+    //getTodayIncome();
+  }, []);
+
+  //get total users
+  function getTotalUsers() {
+    fetch('http://localhost:8080/api/v1/users')
+      .then(response => response.json())
+      .then(data => setTotalUsers(data.total));
+  }
+
+  //get total items
+  function getTotalItems() {
+    fetch('http://localhost:8080/api/v1/items')
+      .then(response => response.json())
+      .then(data => setTotalItems(data.total));
+  }
+
+  //get total today orders and today income
+  function getTodayOrders() {
+    fetch('http://localhost:8080/api/v1/orders?date=today')
+      .then(response => response.json())
+      .then(data => {
+        setTodayOrders(data.total);
+        setTodayIncome(data.totalIncome);
+      });
+  }
+
   return (
     <div>
-        <div class="second">
-         <div class="value"><h1>Today orders</h1>
-            <div class="total"> 
-            
-            </div>
-         </div>
-         <div class="value"><h1>TodayIncome</h1>
-            <div class="total"> 
-            
-            </div>
-         </div>
-         <div class="value"><h1>Total users</h1>
-            <div class="total"> 
-                
-            </div>
-         </div>
-         <div class="value"><h1>Total items</h1>
-            <div class="total"> 
-                
-            </div>
-         </div>
-
+      <div className="second">
+        <div className="value">
+          <h1>Today orders</h1>
+          <div className="total"><h1>{todayOrders}</h1></div>
         </div>
-       
-       
+        <div className="value">
+          <h1>TodayIncome</h1>
+          <div className="total"><h1>{todayIncome}</h1></div>
+        </div>
+        <div className="value">
+          <h1>Total users</h1>
+          <div className="total"><h1>{totalUsers}</h1></div>
+        </div>
+        <div className="value">
+          <h1>Total items</h1>
+          <div className="total"><h1>{totalItems}</h1></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
