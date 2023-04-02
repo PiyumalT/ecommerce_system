3377299acfdef61e45880a22abee1cee9d045700
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-04-02 13:41:59 +0530

    connect to the backend

--- frontend/src/pages/AddProduct/AddProduct.js
+++ frontend/src/pages/AddProduct/AddProduct.js
@@ -2,37 +2,63 @@ import React from 'react';
 import './addProduct.css';
 
 function AddProduct() {
+
+  // send product name, description, price, quantity, category, options and product image to the Spring Boot backend
+  function sendProduct(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('name', e.target.name.value);
+    formData.append('description', e.target.description.value);
+    formData.append('price', e.target.price.value);
+    formData.append('quantity', e.target.quantity.value);
+    formData.append('category', e.target.category.value);
+    formData.append('options', e.target.options.value);
+    formData.append('product_image', e.target.product_image.files[0]);
+
+    fetch('http://localhost:8080/api/products', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
-   
-    <div>
+
+    <div className='middle1'>
       <h1>Add product</h1>
-      <form action="add_item.php" method="post" enctype="multipart/form-data">
+      <form onSubmit={sendProduct}>
         <table border="0">
           <tbody>
             <tr>
               <td className="input4">
                 <h2>Name</h2>
               </td>
-              <td colspan="3">
+              <td colSpan="2">
                 <input type="text" name="name" className="first" />
               </td>
             </tr>
             <tr>
-              <td>
+              <td className="input4">
                 <h2>Description</h2>
               </td>
-              <td colspan="3">
-                <input type="text" name="description" className="second" />
+              <td colSpan="3">
+                <input type="text" name="description" className="second1" />
               </td>
             </tr>
             <tr>
-              <td>
+              <td className="input4">
                 <h2>Price</h2>
               </td>
               <td className="input3">
                 <input type="text" name="price" className="third" />
               </td>
-              <td>
+              <td className="input4">
                 <h2 className="new">Quantity</h2>
               </td>
               <td>
@@ -40,13 +66,13 @@ function AddProduct() {
               </td>
             </tr>
             <tr>
-              <td>
+              <td className="input4">
                 <h2>Category</h2>
               </td>
               <td className="input3">
                 <input type="text" name="category" className="third" />
               </td>
-              <td>
+              <td className="input4">
                 <h2 className="new">Options</h2>
               </td>
               <td>
@@ -54,21 +80,21 @@ function AddProduct() {
               </td>
             </tr>
             <tr>
-              <td>
+              <td className="input4">
                 <h2>Image</h2>
               </td>
-              <td colspan="3">
+              <td colSpan="3">
                 <input
                   type="file"
                   accept="image/jpg"
                   name="product_image"
-                  className="box"
+                  className="box3"
                   style={{ color: 'black' }}
                 />
               </td>
             </tr>
             <tr>
-              <td colspan="4" className="input1">
+              <td colSpan="4" className="input1">
                 <input
                   type="submit"
                   className="btn2"
@@ -81,7 +107,6 @@ function AddProduct() {
         </table>
       </form>
     </div>
-    
   );
 }
 
