7afc4ccd3f2ff0dc35239c73ac08e57f263f5b22
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-04 22:13:54 +0530

    fixed some loose ends.

--- backend/src/main/java/com/ecommercesystem/backend/repository/CartRepository.java
+++ backend/src/main/java/com/ecommercesystem/backend/repository/CartRepository.java
@@ -1,7 +1,6 @@
 package com.ecommercesystem.backend.repository;
 
 import com.ecommercesystem.backend.model.Cart;
-import com.ecommercesystem.backend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,5 +12,5 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
             FROM Cart c
             WHERE c.user_id = ?1
             """)
-    List<Order> findAllCartEntriesByUserId(long id);
+    List<Cart> findAllCartEntriesByUserId(long id);
 }
