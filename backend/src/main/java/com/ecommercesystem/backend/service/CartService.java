2fb0a62e47b44a4617edebf92a9383abf35f4836
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-04 22:15:01 +0530

    add cart service class.

---
+++ backend/src/main/java/com/ecommercesystem/backend/service/CartService.java
@@ -0,0 +1,33 @@
+package com.ecommercesystem.backend.service;
+
+import com.ecommercesystem.backend.exceptionHandler.ResourceNotFoundException;
+import com.ecommercesystem.backend.model.Cart;
+import com.ecommercesystem.backend.repository.CartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final CartRepository cartRepository;
+
+    public Cart addToCart(Cart cart) {
+        return cartRepository.save(cart);
+    }
+
+    public Cart viewFromCartById(long id) {
+        return cartRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("No cart found to the given item id: " + id)
+        );
+    }
+
+    public List<Cart> viewAllCarts() {
+        return cartRepository.findAll();
+    }
+
+    public List<Cart> viewCartByUser(long userId) {
+        return cartRepository.findAllCartEntriesByUserId(userId);
+    }
+}
