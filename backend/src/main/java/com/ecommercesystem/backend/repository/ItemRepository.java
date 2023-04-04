17c982a7ec64598ea744ef26cfe2b695e9da4007
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-04 20:24:47 +0530

    add item update and search by name methods.

--- backend/src/main/java/com/ecommercesystem/backend/repository/ItemRepository.java
+++ backend/src/main/java/com/ecommercesystem/backend/repository/ItemRepository.java
@@ -2,7 +2,24 @@ package com.ecommercesystem.backend.repository;
 
 import com.ecommercesystem.backend.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     // all crud operations handle here - no need to add - already defined
+    @Query("""
+            update item i
+            set i.name = ?2, i.description = ?3, i.price = ?4, i.options = ?5, i.quantity = ?6, i.category = ?7, i.img = ?8
+            where i.id = ?1
+            """)
+    Item updateItem(long itemId, String name, String description, float price, String options, int quantity, String category, String img);
+
+    @Query("""
+            SELECT *
+            FROM item i
+            WHERE i.name LIKE %?1%
+            """)
+    List<Item> searchItemsByName(String name);
+
 }
