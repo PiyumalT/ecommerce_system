7935a655c63762d17aaf4cf0af3a9af4a09da5f9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-04 20:25:51 +0530

    remove all business logic and add search endpoint.

--- backend/src/main/java/com/ecommercesystem/backend/controller/ItemController.java
+++ backend/src/main/java/com/ecommercesystem/backend/controller/ItemController.java
@@ -1,8 +1,7 @@
 package com.ecommercesystem.backend.controller;
 
-import com.ecommercesystem.backend.exceptionHandler.ResourceNotFoundException;
 import com.ecommercesystem.backend.model.Item;
-import com.ecommercesystem.backend.repository.ItemRepository;
+import com.ecommercesystem.backend.service.ItemService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,49 +15,36 @@ import java.util.List;
 @RequestMapping("/api/v1/items")
 public class ItemController {
 
-    private ItemRepository itemRepository;
+    private final ItemService itemService;
 
     @GetMapping("")
     public ResponseEntity<List<Item>> getAllItems() {
-        return ResponseEntity.ok(itemRepository.findAll());
+        return ResponseEntity.ok(itemService.getAllItems());
     }
 
     @PostMapping("/newItem")
-    public ResponseEntity<Item> createItem(@RequestBody Item item) {
-        return ResponseEntity.ok(itemRepository.save(item));
+    public ResponseEntity<Item> createNewItem(@RequestBody Item item) {
+        return ResponseEntity.ok(itemService.createNewItem(item));
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Item> getItemById(@PathVariable("id") long id) {
-        Item item = itemRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("No item found to the given item id: " + id)
-        );
-        return ResponseEntity.ok(item);
+        return ResponseEntity.ok(itemService.getItemById(id));
     }
 
     @PutMapping("{id}")
     public ResponseEntity<Item> updateItem(@PathVariable("id") long id, @RequestBody Item editedItem) {
-        Item itemToUpdate = itemRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("No item found to the given item id: " + id)
-        );
-
-        itemToUpdate.setName(editedItem.getName());
-        itemToUpdate.setDescription(editedItem.getDescription());
-        itemToUpdate.setPrice(editedItem.getPrice());
-        itemToUpdate.setQuantity(editedItem.getQuantity());
-        itemToUpdate.setCategory(editedItem.getCategory());
-
-        itemRepository.save(itemToUpdate);
-
-        return ResponseEntity.ok(itemToUpdate);
+        return ResponseEntity.ok(itemService.updateItem(id, editedItem));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<HttpStatus> deleteItem(@PathVariable("id") long id) {
-        Item item = itemRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("No item found to the given item id: " + id)
-        );
-        itemRepository.delete(item);
+        itemService.deleteItem(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/search={searchTerm}")
+    public ResponseEntity<List<Item>> searchItemByName(@PathVariable("searchTerm") String searchTerm) {
+        return ResponseEntity.ok(itemService.searchItemsByName(searchTerm));
     }
 }
