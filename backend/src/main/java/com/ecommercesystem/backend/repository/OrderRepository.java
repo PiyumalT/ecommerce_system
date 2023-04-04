0ada8870c64c5adc709ff01c28b4fa381e7e3325
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-04 21:51:50 +0530

    add update and find by user and item id methods.

--- backend/src/main/java/com/ecommercesystem/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/ecommercesystem/backend/repository/OrderRepository.java
@@ -2,7 +2,30 @@ package com.ecommercesystem.backend.repository;
 
 import com.ecommercesystem.backend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     // all crud operations handle here - no need to add - already defined
+    @Query("""
+            update Order o
+            set o.address_id = ?2, o.paid_amount = ?3, o.shipped = ?4
+            where o.id = ?1
+            """)
+    Order updateOrder(long orderId, int address_id, float paid_amount, boolean shipped);
+
+    @Query("""
+            SELECT *
+            FROM Order o
+            WHERE o.customer_id = ?1
+            """)
+    List<Order> findAllByUserId(long id);
+
+    @Query("""
+            SELECT *
+            FROM Order o
+            WHERE o.item_id = ?1
+            """)
+    List<Order> findAllByItemId(long id);
 }
