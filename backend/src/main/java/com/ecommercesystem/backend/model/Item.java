4df468e25da2407db2f1856a18f90377f98fcb96
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-04 13:42:36 +0530

    remove unwanted annotations.

--- backend/src/main/java/com/ecommercesystem/backend/model/Item.java
+++ backend/src/main/java/com/ecommercesystem/backend/model/Item.java
@@ -2,37 +2,28 @@ package com.ecommercesystem.backend.model;
 
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
+@Entity
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @Table(name = "item")
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @Column(name = "name")
     private String name;
-    @Column(name = "description")
     private String description;
-    @Column(name = "price")
     private float price;
-
-    @Column(name = "options")
     private String options;
-
-    @Column(name = "qty")
     private int quantity;
-
-    @Column(name = "category")
     private String category;
+    private String img;
 
 }
