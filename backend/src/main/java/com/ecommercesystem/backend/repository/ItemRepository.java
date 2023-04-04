32651e79ef7f5444f3339a438230cfe6efa5340d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-04 21:36:47 +0530

    fixed spelling mistakes of sql command.

--- backend/src/main/java/com/ecommercesystem/backend/repository/ItemRepository.java
+++ backend/src/main/java/com/ecommercesystem/backend/repository/ItemRepository.java
@@ -9,15 +9,15 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     // all crud operations handle here - no need to add - already defined
     @Query("""
-            update item i
+            update Item i
             set i.name = ?2, i.description = ?3, i.price = ?4, i.options = ?5, i.quantity = ?6, i.category = ?7, i.img = ?8
             where i.id = ?1
             """)
     Item updateItem(long itemId, String name, String description, float price, String options, int quantity, String category, byte[] img);
 
     @Query("""
-            SELECT *
-            FROM item i
+            SELECT * 
+            FROM Item i
             WHERE i.name LIKE %?1%
             """)
     List<Item> searchItemsByName(String name);
