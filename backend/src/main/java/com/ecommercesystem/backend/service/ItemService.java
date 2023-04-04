ec8b499394124e628f9d1c6087f8628e07578347
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-04 20:23:53 +0530

    add item service file.

---
+++ backend/src/main/java/com/ecommercesystem/backend/service/ItemService.java
@@ -0,0 +1,45 @@
+package com.ecommercesystem.backend.service;
+
+import com.ecommercesystem.backend.exceptionHandler.ResourceNotFoundException;
+import com.ecommercesystem.backend.model.Item;
+import com.ecommercesystem.backend.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item createNewItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public Item getItemById(long id) {
+        return itemRepository.findById(id).orElseThrow(
+                () -> {
+                    return new ResourceNotFoundException("No item found to the given item id: " + id);
+                }
+        );
+    }
+
+    public Item updateItem(long id, Item editedItem) {
+
+        return itemRepository.updateItem(id, editedItem.getName(), editedItem.getDescription(), editedItem.getPrice(), editedItem.getOptions(), editedItem.getQuantity(), editedItem.getCategory(), editedItem.getImg());
+    }
+
+    public void deleteItem(long id) {
+        Item item = getItemById(id);
+        itemRepository.delete(item);
+    }
+
+    public List<Item> searchItemsByName(String searchTerm) {
+        return itemRepository.searchItemsByName(searchTerm);
+    }
+}
