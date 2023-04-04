8ab17a895f537a41ff0641b067158b6d927c08e1
Tharindu Piyumal <94281006+PiyumalT@users.noreply.github.com>
2023-04-04 20:45:43 +0530

    Fix java 11 not support error.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,56 @@
-import './App.css';
-import Header from './components/Header/Header';
-import { Routes, Route } from 'react-router-dom';
-//import Login from './Login/Login';
-import Home from './pages/Home/Home';
-import Checkout from './components/Checkout/Checkout';
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navabar'
-
-
-
-//import{auth} from './firebase';
-import { useStateValue } from './StateProvider';
-import { useEffect } from 'react';
-import About from './pages/About/About';
-import FAQ from './pages/FAQ/FAQ';
-import Contact_Us from './pages/Contact_Us/Contact_Us';
-import ProductInfo from './pages/ProductInfo/ProductInfo';
-import PlaceOrder from './pages/Checkout/Checkout';
-import OrderController from './components/controller/OrderController';
-import Cart1 from './components/controller/Cart1';
-
-
-
-function App() {
-
-
-  return (
-        <div className="App">
-          <Routes>
-            <Route path="/checkout" element={<><Header/><Checkout/></>}/>
-            {/*<Route path="/login" element={<Login/>}/>*/}
-            <Route path="/" element={<><Header/><Navbar/><Home/><Footer/></>}/>
-            <Route path="/About" element={<><About/></>}/>
-            <Route path="/FAQ" element={<><FAQ/></>}/>
-            <Route path="/Contact_Us" element={<><Contact_Us/></>}/>
-
-            <Route path="/ProductInfo/:itemID" element={<><ProductInfo/></>}/>
-            
-            <Route path="/PlaceOrder" element={<><PlaceOrder/></>}/>
-            <Route path="/Orderc" element={<><OrderController/></>}/>
-            <Route path="/cart1" element={<><Cart1/></>}/>
-
-
-
-
-          </Routes>
-          
-          
-            
-          
-
-          
-        </div>
-  );
-  
-}
-
+import './App.css';
+import Header from './components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
+//import Login from './Login/Login';
+import Home from './pages/Home/Home';
+import Checkout from './components/Checkout/Checkout';
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navabar'
+
+
+
+//import{auth} from './firebase';
+import { useStateValue } from './StateProvider';
+import { useEffect } from 'react';
+import About from './pages/About/About';
+import FAQ from './pages/FAQ/FAQ';
+import Contact_Us from './pages/Contact_Us/Contact_Us';
+import ProductInfo from './pages/ProductInfo/ProductInfo';
+import PlaceOrder from './pages/Checkout/Checkout';
+import OrderController from './components/controller/OrderController';
+import Cart1 from './components/controller/Cart1';
+function App() {
+
+
+  return (
+        <div className="App">
+          <Routes>
+            <Route path="/checkout" element={<><Header/><Checkout/></>}/>
+            {/*<Route path="/login" element={<Login/>}/>*/}
+            <Route path="/" element={<><Header/><Navbar/><Home/><Footer/></>}/>
+            <Route path="/About" element={<><About/></>}/>
+            <Route path="/FAQ" element={<><FAQ/></>}/>
+            <Route path="/Contact_Us" element={<><Contact_Us/></>}/>
+
+            <Route path="/ProductInfo/:itemID" element={<><ProductInfo/></>}/>
+            
+            <Route path="/PlaceOrder" element={<><PlaceOrder/></>}/>
+            <Route path="/Orderc" element={<><OrderController/></>}/>
+            <Route path="/cart1" element={<><Cart1/></>}/>
+
+
+
+
+          </Routes>
+          
+          
+            
+          
+
+          
+        </div>
+  );
+  
+}
+
 export default App;
