5e5050de92a72baad28c9c5447897ddee0c39b3d
NimeshaKavindi <91362972+NimeshaKavindi@users.noreply.github.com>
2023-04-05 08:26:15 +0530

    upload register page with some changes

--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -1,56 +1,66 @@
-import React from 'react'
-import Footer from '../../components/Footer/Footer'
-import Header from '../../components/Header/Header'
-import Navbar from '../../components/Navbar/Navabar'
-import './register.css'
+import React, { useState } from 'react';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navabar';
+import './register.css';
 import { Link } from 'react-router-dom';
 
-
-
- 
 export default function Register() {
-    return (
-      <div>
-          <Header/>
-          <Navbar/>
-          <div className="middle">
-            
-                <div className="box">
-                    <h1>Create New account</h1>
-                    <label for ="email">E-mail:</label>
-                    <br/>
-                    <input type="text" name="email"/>
-                    <br/>
-                    <label for="password">Password:</label>
-                    <br/>
-                    <input type="password" name="password"/>
-                    <br/>
-                    <label for="confirm password">Confirm Password:</label>
-                    <br/>
-                    <input type="password" name="conpassword"/>
-                    <br/>
-                    <br/>
-                    <Link to="./Emailsent">
-                        <input type="submit" value="Create account" name="submit"/>
-                    </Link>
-                    <br/>
-                    <div>
-                    <p>Don't have an account? <Link to="./SignIn">Already have an account</Link></p>
-                    </div>
-                   
-                    
+  const [email, setEmail] = useState('');
 
-                    
-
-                    
-                    
-                    
-                    
-                </div>
-          
-          </div>
-          <Footer/>
-      </div>
-    )
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
   }
-  
+
+  function validateEmail() {
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
+      alert('Invalid email address');
+      return false;
+    }
+    return true;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (validateEmail()) {
+      // handle form submission
+      console.log('Valid email');
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <div className="middle">
+        <div className="box">
+          <h1>Create New account</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">E-mail:</label>
+            <br />
+            <input type="text" name="email" value={email} onChange={handleEmailChange} />
+            <br />
+            <label htmlFor="password">Password:</label>
+            <br />
+            <input type="password" name="password" />
+            <br />
+            <label htmlFor="confirm password">Confirm Password:</label>
+            <br />
+            <input type="password" name="conpassword" />
+            <br />
+            <br />
+            <input type="submit" value="Create account" name="submit" />
+          </form>
+          <br />
+          <div>
+            <p>
+              Don't have an account? <Link to="./SignIn">Already have an account</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
