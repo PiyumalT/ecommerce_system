dd5e7dc284448b1547148ec0298c5b766f2f70c2
Tharindu Piyumal <94281006+PiyumalT@users.noreply.github.com>
2023-04-05 12:21:26 +0530

    user account control section added

--- frontend/src/pages/ProductInfo/ProductInfo.js
+++ frontend/src/pages/ProductInfo/ProductInfo.js
@@ -1,16 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navabar';
 import './ProductInfo.css';
 
+
 const ProductDetails = () => {
   // State for quantity of the item
   const [quantity, setQuantity] = useState(1);
+  const [item, setItem] = useState([]);
+  const [item_opt, setItemopt] = useState("");
 
   // Function to handle increasing the quantity
+  
+
+  function getProductId(url) {
+    const urlParts = url.split('/');
+    const productInfoIndex = urlParts.indexOf('ProductInfo');
+    
+    if (productInfoIndex !== -1) {
+      const nextIndex = productInfoIndex + 1;
+      if (nextIndex < urlParts.length) {
+        return(urlParts[nextIndex]);
+      } else {
+        return("0");
+      }
+    } else {
+      return("0");
+    }
+  }
+
+  const product_url = "http://localhost:8080/api/v1/items/"+(getProductId(window.location.href));
+  useEffect(() => {
+    fetch(product_url)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Network response was not ok");
+        }
+      })
+      .then(result => {
+        setItem(result);
+        setItemopt(result.options);
+      })
+      .catch(error => {
+        //console.error("Fetch Error:", error);
+        //add error page here.
+        window.location.href = "../error";
+      });
+  }, []);
+  
   const handleIncrease = () => {
-    setQuantity(quantity + 1);
+    if (quantity < item.quantity){
+      setQuantity(quantity + 1);
+    }
   };
 
   // Function to handle decreasing the quantity
@@ -20,7 +64,18 @@ const ProductDetails = () => {
     }
   };
 
-  function generateOptions(optionsList) {
+  
+
+
+  function generateOptions(options) {
+    if (options===''){
+      options='Default';
+    }
+    const optionsList = (options).split(',').map(option => option.trim());
+    if (optionsList.length === 0) {
+      optionsList.push('Default');
+    }
+    //setSelectedOption(optionsList[0]);
     return optionsList.map((option, index) => (
       <option key={index} value={option}>
         {option}
@@ -28,22 +83,36 @@ const ProductDetails = () => {
     ));
   }
 
-  //give backend options to here
-  const optionsList = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
-  const options = generateOptions(optionsList);
-
-  //remove later
-  function handleOptionChange(event) {
-    const selectedOption = event.target.value;
-    alert(`Selected option: ${selectedOption}`);
-  }
-
   function handleQuantityChange(event) {
     const value = Number(event.target.value);
-    if (!isNaN(value) && value > 0 && value <= 100) {
+    if (!isNaN(value) && value > 0 && value <= item.quantity) {
       setQuantity(value);
     }
   }
+  
+    //give backend options to here
+
+    //const optionsList = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];y
+    var options = generateOptions(item_opt);
+
+    const [selectedOption, setSelectedOption] = useState("Default");
+    const handleOptionChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+    
+    
+    const handlePlaceOrderClick = () => {
+      const data = {
+        productid: (getProductId(window.location.href)),
+        option: selectedOption,
+        quantity: quantity
+      };
+      const urlEncodedData = Object.keys(data).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
+      window.location.href = `/placeorder?${urlEncodedData}`;
+    };
+
+
+
 
   return (
     <>
@@ -54,10 +123,13 @@ const ProductDetails = () => {
             <img src="/Picture/5.jpg" alt="Item" />
         </div>
         <div className="product-details">
-          <h1 className="product-name">Item Name</h1>
-          <p className="product-description">Item Description</p>
-          <p className="product-price">$10.00</p>
-          <select className="product-options" onChange={handleOptionChange}>
+          <h1 className="product-name">{item.name}</h1>
+
+          <p className="product-description">{item.description}</p>
+          <p className="product-price">Rs.{item.price}</p>
+          <select className="product-options" 
+            value={selectedOption} 
+            onChange={handleOptionChange}>
             {options}
           </select>
           <div className="quantity">
@@ -75,7 +147,7 @@ const ProductDetails = () => {
             </button>
           </div>
           <div className="buttons">
-            <button className="buy-now">Buy Now</button>
+            <button className="buy-now" onClick={handlePlaceOrderClick}>Buy Now</button>
             <button className="add-to-cart">Add to Cart</button>
           </div>
         </div>
