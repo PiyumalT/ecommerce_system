bfb1b0036a50689cecf808874f3a0c479a6b9ff3
Tharindu Piyumal <94281006+PiyumalT@users.noreply.github.com>
2023-04-06 16:59:05 +0530

    Bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 //import Login from './Login/Login';
 import Home from './pages/Home/Home';
-import Checkout from './components/Checkout/Checkout';
+import AddCart from './components/AddCart/AddCart';
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navabar'
 
@@ -35,7 +35,7 @@ function App() {
         <div className="App">
           <div>
             <Routes>
-            <Route path="/checkout" element={<><Header/><Checkout/></>}/>
+            <Route path="/addcart" element={<><Header/><AddCart/></>}/>
             {/*<Route path="/login" element={<Login/>}/>*/}
             <Route path="/" element={<><Header/><Navbar/><Home/><Footer/></>}/>
             <Route path="/About" element={<><About/></>}/>
@@ -43,6 +43,8 @@ function App() {
             <Route path="/ProductInfo" element={<><ProductInfo/></>}/>
             <Route path="/Contact_Us" element={<><Contact_Us/></>}/>
             <Route path="/SignIn" element={<><SignIn/></>}/>
+            <Route path="/register" element={<><Register/></>}/>
+
             <Route path="/Emailsent" element={<><Emailsent/></>}/>
             <Route path="/userAccount" element={<><UserAccountHome/></>}/>
             <Route path="/ViewOrders" element={<><ViewOrders/></>}/>
@@ -51,7 +53,7 @@ function App() {
           </Routes>
           </div>
           
-          <Register/>
+          
           
           
           
