74d4c5928e52fadec8f04b06e91b41fd72f98e6a
Tharindu Piyumal <94281006+PiyumalT@users.noreply.github.com>
2023-04-06 22:25:00 +0530

    add java 11 support , db username hardcoded.

--- backend/src/main/java/com/ecommercesystem/backend/repository/ItemRepository.java
+++ backend/src/main/java/com/ecommercesystem/backend/repository/ItemRepository.java
@@ -8,18 +8,17 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     // all crud operations handle here - no need to add - already defined
-    @Query("""
-            update Item i
-            set i.name = ?2, i.description = ?3, i.price = ?4, i.options = ?5, i.quantity = ?6, i.category = ?7, i.img = ?8
-            where i.id = ?1
-            """)
+    @Query("update Item i\n" +
+            "set i.name = ?2, i.description = ?3, i.price = ?4, i.options = ?5, i.quantity = ?6, i.category = ?7, i.img = ?8\n" +
+            "where i.id = ?1")
+
     Item updateItem(long itemId, String name, String description, float price, String options, int quantity, String category, byte[] img);
 
-    @Query("""
-            SELECT * 
-            FROM Item i
-            WHERE i.name LIKE %?1%
-            """)
+    @Query("SELECT i \n" +
+            "FROM Item i\n" +
+            "WHERE i.name LIKE %?1%")
+    List<Item> findItemsByNameContaining(String name);
+
     List<Item> searchItemsByName(String name);
 
 }
