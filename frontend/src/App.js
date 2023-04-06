5c52dc2388f79969470eee3cd9d147794083b6fb
Tharindu Piyumal <94281006+PiyumalT@users.noreply.github.com>
2023-04-06 20:41:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,9 @@ import Header from './components/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 //import Login from './Login/Login';
 import Home from './pages/Home/Home';
-import Checkout from './components/Checkout/Checkout';
+import AddCart from './components/AddCart/AddCart';
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navabar'
-
 
 
 
@@ -15,7 +14,15 @@ import { useStateValue } from './StateProvider';
 import { useEffect } from 'react';
 import About from './pages/About/About';
 import FAQ from './pages/FAQ/FAQ';
+import ProductInfo from './pages/ProductInfo/ProductInfo';
 import Contact_Us from './pages/Contact_Us/Contact_Us';
+import Register from './pages/Register/Register';
+import SignIn from './pages/SignIn/SignIn';
+import Emailsent from './pages/Emailsent/Emailsent';
+import UserAccountHome from './pages/UserAccount/UserAccountHome';
+import ViewOrders from './pages/UserAccount/ViewOrders';
+import EditUserInfo from './pages/UserAccount/EditUserinfo';
+
 
 import AdminNavBar from './components/AdminNavBar/AdminNavBar';
 import AdminDashbord from './pages/AdminDashbord/AdminDashbord';
@@ -27,24 +34,41 @@ import SalesList from './pages/SalesList/SalesList';
 
 
 
+
+
 function App() {
 
 
   return (
         <div className="App">
-          <Routes>
-            <Route path="/checkout" element={<><Header/><Checkout/></>}/>
+          <div>
+            <Routes>
+            <Route path="/addcart" element={<><Header/><AddCart/></>}/>
             {/*<Route path="/login" element={<Login/>}/>*/}
             <Route path="/" element={<><Header/><Navbar/><Home/><Footer/></>}/>
             <Route path="/About" element={<><About/></>}/>
             <Route path="/FAQ" element={<><FAQ/></>}/>
+            <Route path="/ProductInfo" element={<><ProductInfo/></>}/>
             <Route path="/Contact_Us" element={<><Contact_Us/></>}/>
+            <Route path="/SignIn" element={<><SignIn/></>}/>
+            <Route path="/register" element={<><Register/></>}/>
+
+            <Route path="/Emailsent" element={<><Emailsent/></>}/>
+            <Route path="/userAccount" element={<><UserAccountHome/></>}/>
+            <Route path="/ViewOrders" element={<><ViewOrders/></>}/>
+            <Route path="/EditUserInfo" element={<><EditUserInfo/></>}/>
+
             <Route path="/AdminDashbord" element={<><AdminNavBar/><AdminDashbord/><Footer/></>}/>
             <Route path="/AddProduct" element={<><AdminNavBar/><AddProduct/><Footer/></>}/>
             <Route path="/ProductList" element={<><AdminNavBar/><ProductList/><Footer/></>}/>
             <Route path="/UsersList" element={<><AdminNavBar/><UsersList/><Footer/></>}/>
             <Route path="/SalesList" element={<><AdminNavBar/><SalesList/><Footer/></>}/>
+
           </Routes>
+          </div>
+          
+          
+          
           
           
             
