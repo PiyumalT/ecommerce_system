f7572d4b57fc4ec39258d8c13fcc3b4d290be86a
NimeshaKavindi <91362972+NimeshaKavindi@users.noreply.github.com>
2023-04-08 10:24:14 +0530

    validate the user email

--- frontend/src/pages/SignIn/SignIn.js
+++ frontend/src/pages/SignIn/SignIn.js
@@ -1,47 +1,78 @@
-import React from 'react'
-import Footer from '../../components/Footer/Footer'
-import Header from '../../components/Header/Header'
-import Navbar from '../../components/Navbar/Navabar'
-import './signin.css'
- 
+import React from 'react';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navabar';
+import './signin.css';
+import { useState } from 'react';
+
 export default function SignIn() {
-    return (
-        <>
-      <div>
-          <Header/>
-          <Navbar/>
-          <div className="middle1">
-             <div className="box1">
-                    <h1>Log In</h1>
-                    <label for="email">Email:</label>
-                    <br/>
-                    <div class="email_field">
-                        <input type="email" name="email"/>
-                    </div>
-                    <br/>
-                    <label for="password">Password</label>
-                    <br/>
-                    <div class="pass_field">
-                        <input type="password" name="password"/>
-                    </div>
-                    <br/>
-                    <br/> 
-                    <div class="remember-me">
-                         <input type="checkbox" name="remember-me" />
-                         <label for="remember-me">Remember Me</label>
-                    </div>
-                    <button id="login-btn" type="submit">login</button>
-                    <div class="ps-forgot"><a href="#">Forgot password?</a></div>
-                    <div class="ps-forgot"><a href="./Register">Not have an account? Sign Up now.</a></div>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-             </div>    
-          </div>
-         
-      </div>
-       <Footer/>
-       </>
-    )
-  }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
   
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-          
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    const data = { email, password };
+  
+    fetch('http://localhost:8080/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // handle the response from the server here
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <Header />
+        <Navbar />
+        <div className="middle1">
+          <div className="box1">
+            <h1>Log In</h1>
+            <label htmlFor="email">Email:</label>
+            <br />
+            <div className="email_field">
+              <input type="email" name="email" value={email} onChange={handleEmailChange} />
+            </div>
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <div className="pass_field">
+              <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+            </div>
+            <br />
+            <br /> 
+            <div className="remember-me">
+              <input type="checkbox" name="remember-me" />
+              <label htmlFor="remember-me">Remember Me</label>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <button id="login-btn" type="submit">login</button>
+            </form>
+            <div className="ps-forgot"><a href="#">Forgot password?</a></div>
+            <div className="ps-forgot"><a href="./Register">Not have an account? Sign Up now.</a></div>
+          </div>    
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
