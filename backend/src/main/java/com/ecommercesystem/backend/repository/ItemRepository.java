9d4e28d2a08f7c2c564f5f6406668e8e77a0c0a2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-08 11:47:08 +0530

    reformat the code and remove redundant code.

--- backend/src/main/java/com/ecommercesystem/backend/repository/ItemRepository.java
+++ backend/src/main/java/com/ecommercesystem/backend/repository/ItemRepository.java
@@ -11,14 +11,11 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query("update Item i\n" +
             "set i.name = ?2, i.description = ?3, i.price = ?4, i.options = ?5, i.quantity = ?6, i.category = ?7, i.images = ?8\n" +
             "where i.item_id = ?1")
-
     Item updateItem(long itemId, String name, String description, float price, String options, int quantity, String category, String img);
 
     @Query("SELECT i \n" +
             "FROM Item i\n" +
             "WHERE i.name LIKE %?1%")
-    List<Item> findItemsByNameContaining(String name);
-
     List<Item> searchItemsByName(String name);
 
 }
